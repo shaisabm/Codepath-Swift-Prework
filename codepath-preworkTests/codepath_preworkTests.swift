a4f58dfe10eda4b7edb9b1e39b5375a3bac773a7
Shaisab Mistry <shaisabmistry@gmail.com>
2024-11-21 13:27:53 -0500

    Initial Commit

---
+++ codepath-preworkTests/codepath_preworkTests.swift
@@ -0,0 +1,17 @@
+//
+//  codepath_preworkTests.swift
+//  codepath-preworkTests
+//
+//  Created by shaisabm on 11/21/24.
+//
+
+import Testing
+@testable import codepath_prework
+
+struct codepath_preworkTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
